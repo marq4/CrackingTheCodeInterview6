e2d1c7dce5023464ae625aaf4636d0a62efe269f
jmarquin <jmarquin@JMARQUIN-LAP.oradev.oraclecorp.com>
2021-11-10 12:00:15 -0600

    1.2 Permutation (Java)

---
+++ Java/1Ch_ArraysStrings/2Permutation.java
@@ -0,0 +1,33 @@
+/*
+ * To try it out: javac 2Permutation.java ; java Permutation "yyy" "yyy" 
+ * Assumes any printable characters other than space are valid. 
+ *
+ * Instead of thinking of chars we can imagine we're comparing the digits of 2 numbers: 
+ *   "abc" "cba" -> [0,1,2] [2,1,0]. 
+ * We could get a "digest" of each big number by adding together all its digits, 
+ *   then do the same for the other and finally substract the results: res1 - res2. 
+ *   If that is 0 then both numbers must have been composed of the same digts. 
+ * Instead we can also substract each pair of digits at the same position in each big number and add all up: 
+ *   (0 - 2) + (1 - 1) + (2 - 0) = 0. 
+ *   "da...e" "da...f" => (3 - 3) + (0 - 0) + ... + (4 - 5) = 1. 
+*/
+
+class Permutation {
+	static boolean isPermutation(String first, String second) {
+                String f = first.replaceAll("\\s", "");
+                String s = second.replaceAll("\\s", "");
+
+		if (f.length() != s.length()) return false; 
+
+		int sum = 0;
+		for (int q = 0; q < f.length(); q++) 
+			sum += ( f.charAt(q) - s.charAt(q) ); 
+
+		return (sum == 0); 
+	}
+
+	public static void main(String Args[]) {
+		System.out.println("Is permutation?   " + Args[0] + "   " + Args[1] + "   =>" + isPermutation(Args[0], Args[1]) ); 
+	}
+}
+
