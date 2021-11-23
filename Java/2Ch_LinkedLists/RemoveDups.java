d64fb838caad46a8717937a5db7ac5c72cd40d19
jmarquin <jmarquin@JMARQUIN-LAP.oradev.oraclecorp.com>
2021-11-22 20:11:09 -0600

    2.1 Remove SLL Duplicates (Java).

---
+++ Java/2Ch_LinkedLists/RemoveDups.java
@@ -0,0 +1,45 @@
+/*
+ * Remove duplicates of an <int> Simply Linked List. 
+ * Node Curr begins at the head of the list until we reach the end of the list (null). 
+ * Node Runner is what we use to compare with Curr to find duplicates. 
+ * Node Prev is needed to connect the first half list to the remainer of the list after 
+ * 	deleting a node. 
+ * While we haven't reached the end of the list: 
+ * 	if we find a duplicate we need to: 
+ * 		+ Connect previous to the next of the one we're about to delete. 
+ * 		+ Help gc to clear memory of duplicated node. 
+ * 		+ Continue cheking the rest of the list. 
+ * 	otherwise just continue searching. 
+ */
+
+class SLLRemoveDups {
+	static void removeDups(SimplyLinkedList SLL) {
+		SimplyLinkedList.Node Curr, Runner, Prev; 
+		Curr = SLL.Head; 
+		while (Curr != null) {
+			Prev = Curr; 
+			Runner = Curr.Next; 
+			while (Runner != null) {
+				if (Curr.data == Runner.data) { //delete dup: 
+					Prev.Next = Runner.Next; 
+					Runner = null; 
+					Runner = Prev.Next; 
+				} else {
+					Runner = Runner.Next; 
+					Prev = Prev.Next; 
+				}
+			}
+			Curr = Curr.Next; 
+		}
+	}
+
+	public static void main(String[] Args) {
+		if (Args == null || Args.length < 1) return; 
+		SimplyLinkedList TestCase = new SimplyLinkedList(); 
+		for (int a = 0; a < Args.length; a++) TestCase.add( Integer.parseInt(Args[a]) ); 
+		System.out.println("Before: " + TestCase); 
+		removeDups(TestCase); 
+		System.out.println("After:  " + TestCase); 
+	}
+}
+
---
+++ Java/2Ch_LinkedLists/SimplyLinkedList.java
@@ -0,0 +1,54 @@
+
+class SimplyLinkedList {
+	Node Head;
+
+	SimplyLinkedList() {
+		this.Head = null; 
+	}
+
+	SimplyLinkedList(int firstData) {
+		this.Head = new Node(firstData); 
+	}
+
+	boolean isEmpty() {
+		return (this.Head == null); 
+	}
+
+	void add(int lastData) {
+		if ( this.isEmpty() ) 
+			this.Head = new Node(lastData); 
+		else {
+			Node Trav = this.Head; 
+			while (Trav.Next != null) 
+				Trav = Trav.Next; 
+			Trav.Next = new Node(lastData);
+		}
+	}
+
+
+	class Node {
+		Node Next; 
+		int data;  
+
+		Node(int firstData) {
+			this.Next = null; 
+			this.data = firstData; 
+		}
+
+		public String toString() {
+			return "[SLLNode] ?->(" + String.valueOf(this.data) + ")->?"; 
+		}
+	}
+
+	public String toString() {
+		Node Trav = Head; 
+		String Repr = "SLL => "; 
+		while (Trav != null) {
+			Repr += "(" + String.valueOf(Trav.data) + ")->"; 
+			Trav = Trav.Next; 
+		}
+		Repr += "|| ";
+		return Repr; 
+	}
+}
+
